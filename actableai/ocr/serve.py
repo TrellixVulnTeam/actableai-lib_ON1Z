8ec0051e9fff81afd034c4e4f6bc48e77f03ace2
DemyCode <verycols@gmail.com>
2022-11-02 15:39:04 +0100

    fix: Implemented __call__ to have request from ray url

--- actableai/ocr/serve.py
+++ actableai/ocr/serve.py
@@ -1,8 +1,9 @@
 from PIL import Image
-
+from io import BytesIO
+import base64
 
 from actableai.serve.abstract_serve import AbstractRayDeployment
-
+import starlette.requests
 
 class AAIOCRExtractor(AbstractRayDeployment):
     def __init__(self):
@@ -23,9 +24,15 @@ class AAIOCRExtractor(AbstractRayDeployment):
 
         self.detector = Predictor(config)
 
-    def predict(self, img_path: str) -> str:
+    def predict(self, img_byte):
         """
         TODO write documentation
         """
-        img = Image.open(img_path)
+        img = Image.open(img_byte)
         return self.detector.predict(img)
+
+    def __call__(self, request: starlette.requests.Request):
+        request_body = request.json()
+        img_base64 = request_body["img"]
+        img = BytesIO(base64.b64decode(img_base64))
+        return self.predict(img)
